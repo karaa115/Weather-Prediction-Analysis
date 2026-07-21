c93b6894d8747531f42590d4f889aba67ef04913
karaa115 <161724991+karaa115@users.noreply.github.com>
2026-07-21 21:19:07 +0200

    Create train_model.py for weather prediction
    Added train_model.py for weather prediction project.

---
+++ train_model.py
@@ -0,0 +1,10 @@
+"""
+Weather Prediction Project
+
+Author: Karolina Kwietniewska
+
+This file is included to make the repository Python-oriented.
+The complete analysis is available in the Jupyter Notebook.
+"""
+
+print("Weather Prediction Analysis")
